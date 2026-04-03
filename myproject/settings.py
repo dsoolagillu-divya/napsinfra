4de75ca25a8059b207b773f5a87badcf828bab86
dsoolagillu-divya <dsoolagillu@gmail.com>
2026-04-04 01:24:29 +0530

    Updated ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l!gla%=jee+zjhl54f$*i$(p*=@%#4i12pn*=1s^33ced_t37a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.90.245.182', 'localhost', '127.0.0.1']
 
 
 # Application definition
